6da0d3966a74330de8927e059ba9b8f4c46c2e67
JuniorPalomino <junior.palomino@ucsm.edu.pe>
2022-08-27 21:38:16 -0500

    NuevaSubidaActividades

--- P02-ClasesYObjetos/Actividades/Punto b/miproyect/src/miproyect/Miproyect.java
+++ P02-ClasesYObjetos/Actividades/Punto b/miproyect/src/miproyect/Miproyect.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package miproyect;
+
 import java.util.*;
+
 /**
  *
  * @author Alumno
@@ -16,7 +18,7 @@ public class Miproyect {
     public static void main(String[] args) {
         // TODO code application logic here
         //////////----PUNTO A----////////
-        perro fido =new perro("chihuahua", "pekines");
+        perro fido = new perro("chihuahua", "marron");
         fido.increEdad(2);
         System.out.println("--------FIDO--------");
         System.out.println(fido.getRaza());
@@ -24,15 +26,16 @@ public class Miproyect {
         System.out.println(fido.getEdad());
         System.out.println("--------DATOS COMPLETOS --------");
         System.out.println(fido.toString());
- 
-        perro pelusa =new perro("pequines","blanco");
+
+        perro pelusa = new perro("pequines", "blanco");
         System.out.println("--------PELUSA--------");
-        pelusa.increEdad(6);
+        pelusa.increEdad(5);
         System.out.println(pelusa.getRaza());
         System.out.println(pelusa.getColor());
         System.out.println(pelusa.getEdad());
         System.out.println("--------DATOS COMPLETOS --------");
         System.out.println(pelusa.toString());
+        
         
         //////////----PUNTO B----////////
         
@@ -41,44 +44,44 @@ public class Miproyect {
         Mascotas.aptoVacunaA(fido);
         System.out.println("--------DIAGNOSTICO DE PELUSA --------");
         Mascotas.aptoVacunaA(pelusa);
-  
         
-       //////////----PUNTO C----////////
-       //System.out.println("--------INGRESO DE DATOS PARA EL NUEVO DIAGNOSTICO DE FIDO --------");
-       // Scanner sc =new Scanner(System.in); 
+        /*
+        //////////----PUNTO C----////////
         
-       // System.out.println("Ingrese el peso del perro: "); 
+        System.out.println("--------INGRESO DE DATOS PARA EL NUEVO DIAGNOSTICO DE FIDO --------");
+        Scanner sc = new Scanner(System.in);
 
-        //fido.peso=sc.nextDouble();
-        
-       // System.out.println("Ingrese la edad del perro: "); 
+        System.out.println("Ingrese el nuevo peso del perro: ");
 
-       // fido.setEdad(sc.nextInt());
+        fido.peso = sc.nextDouble();
 
-        //System.out.println("Ingrese la temperatura del perro: "); 
+        System.out.println("Ingrese la nueva edad del perro: ");
 
-        //fido.setTemp(sc.nextDouble()); 
-        
-        //Mascotas.aptoVacunaB(fido.peso, fido.getEdad(), fido.getTemp());
-        
-        
-        //System.out.println("--------INGRESO DE DATOS PARA EL NUEVO DIAGNOSTICO DE PELUSA --------");
-        
-        //System.out.println("Ingrese el peso del perro: "); 
+        fido.setEdad(sc.nextInt());
 
-        //pelusa.peso=sc.nextDouble();
-        
-        //System.out.println("Ingrese la edad del perro: "); 
+        System.out.println("Ingrese la nueva temperatura para el perro: ");
 
-        //pelusa.setEdad(sc.nextInt());
+        fido.setTemp(sc.nextDouble());
 
-        //System.out.println("Ingrese la temperatura del perro: "); 
+        Mascotas.aptoVacunaB(fido.peso, fido.getEdad(), fido.getTemp());
 
-        //pelusa.setTemp(sc.nextDouble()); 
-        
-        //Mascotas.aptoVacunaB(pelusa.peso, pelusa.getEdad(), pelusa.getTemp());
-        
-        
+        System.out.println("--------INGRESO DE DATOS PARA EL NUEVO DIAGNOSTICO DE PELUSA --------");
+
+        System.out.println("Ingrese el nuevo peso del perro: ");
+
+        pelusa.peso = sc.nextDouble();
+
+        System.out.println("Ingrese la nueva edad del perro: ");
+
+        pelusa.setEdad(sc.nextInt());
+
+        System.out.println("Ingrese la nueva temperatura para el perro: ");
+
+        pelusa.setTemp(sc.nextDouble());
+
+        Mascotas.aptoVacunaB(pelusa.peso, pelusa.getEdad(), pelusa.getTemp());
+        */
+
     }
-    
+
 }
